6fed4227148162e6d6c23351fd12123ab9cda868
Raghav Thakur <raghavthakur.dev@gmail.com>
2025-05-25 22:30:38 -0700

    Add database connection

--- backend/ai_blog_app/ai_blog_app/settings.py
+++ backend/ai_blog_app/ai_blog_app/settings.py
@@ -73,11 +73,21 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',  # Replace with your database name
+        'PASSWORD': '',  # Replace with your database password
+        'HOST': '',  # Replace with your database host
+        'PORT': '',  # Replace with your database port
     }
 }
 
